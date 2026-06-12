d13ae6099da4ea77e8e49f554aad3ebf0b9be7bf
Abubakar Unwaha <abubakarunwaha@gmail.com>
2026-06-12 11:28:03 +0100

    DOM Elements selected

---
+++ script.js
@@ -0,0 +1,33 @@
+// Form Elements
+
+const assignmentForm = document.getElementById("assignmentForm");
+
+const assignmentInput = document.getElementById("assignmentInput");
+
+const courseInput = document.getElementById("courseInput");
+
+const priorityInput = document.getElementById("priorityInput");
+
+// Assignment List
+
+const assignmentList = document.getElementById("assignmentList");
+
+const emptyMsg = document.getElementById("emptyMsg");
+
+// Statistics
+
+const totalCount = document.getElementById("totalCount");
+
+const pendingCount = document.getElementById("pendingCount");
+
+const completedCount = document.getElementById("completedCount");
+
+// Filters
+
+const filterButtons = document.querySelectorAll(".filter-btn");
+console.log(assignmentForm);
+console.log(assignmentInput);
+console.log(courseInput);
+console.log(priorityInput);
+console.log(assignmentList);
+console.log(filterButtons);
